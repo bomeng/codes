490b293db9f48de669e322270e8161f7ec5e0c5a
bomeng <bmeng@us.ibm.com>
2017-09-27 14:52:01 -0700

    solution

---
+++ hackerrank/Algorithms/Warmup/Birthday Cake Candles/Solution.scala
@@ -0,0 +1,22 @@
+import scala.util.Sorting
+
+object Solution {
+
+  def birthdayCakeCandles(n: Int, ar: Array[Int]): Int = {
+    // Complete this function
+    val max = ar.max
+    ar.count(_ == ar.max)
+  }
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner(System.in)
+    val n = sc.nextInt()
+    val ar = new Array[Int](n)
+    for (ar_i <- 0 until n) {
+      ar(ar_i) = sc.nextInt()
+    }
+    val result = birthdayCakeCandles(n, ar)
+    println(result)
+  }
+}
+
