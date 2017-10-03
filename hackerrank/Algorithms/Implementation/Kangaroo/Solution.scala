7eadb037866d1d36a303ee9c7a77588a69090021
bomeng <bmeng@us.ibm.com>
2017-10-03 11:36:28 -0700

    add

---
+++ hackerrank/Algorithms/Implementation/Kangaroo/Solution.scala
@@ -0,0 +1,26 @@
+object Solution {
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner(System.in)
+    val x1 = sc.nextInt()
+    val v1 = sc.nextInt()
+    val x2 = sc.nextInt()
+    val v2 = sc.nextInt()
+
+    if (v1 == v2) {
+      println("NO")
+    } else {
+      if (v1 <= v2) {
+        println("NO")
+      } else {
+        if ((x2 - x1) % (v1 - v2) == 0) {
+          println("YES")
+        }
+        else {
+          println("NO")
+        }
+      }
+    }
+  }
+}
+
