47598ee096e246984c4540b82fcb409e252f69cf
Bo Meng <bo.meng@sysdig.com>
2020-05-26 10:17:45 -0700

    lc

---
+++ java/leetcode/src/solution_525/Solution.java
@@ -0,0 +1,31 @@
+package solution_525;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int findMaxLength(int[] nums) {
+        int max = 0;
+        if (nums.length == 1 || nums.length == 0) {
+            return max;
+        }
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                sum += 1;
+            } else {
+                sum -= 1;
+            }
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            } else {
+                max = Math.max(max, i - map.get(sum));
+            }
+            max = Math.max(max, i + 1);
+        }
+        return max;
+    }
+}
