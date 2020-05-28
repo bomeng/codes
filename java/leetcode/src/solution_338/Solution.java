f08d01d362f43b32b845a4c5414429bc3acc9b02
Bo Meng <bo.meng@sysdig.com>
2020-05-28 09:24:32 -0700

    lc

--- java/leetcode/src/solution_338/Solution.java
+++ java/leetcode/src/solution_338/Solution.java
@@ -5,7 +5,11 @@ public class Solution {
     public int[] countBits(int num) {
         int[] result = new int[num + 1];
         for (int i = 1; i <= num; i++) {
-            result[i] = result[i & (i - 1)] + 1;
+            if (i % 2 == 0) {
+                result[i] = result[i / 2] + 1;
+            } else {
+                result[i] = result[i - 1] + 1;
+            }
         }
 
         return result;
