1b8db39cdc41a11e4c6add680f6c2354da33381a
Bo Meng <bo.meng@sysdig.com>
2020-06-01 13:09:34 -0700

    lc

---
+++ java/leetcode/src/solution_1460/Solution.java
@@ -0,0 +1,12 @@
+package solution_1460;
+
+import java.util.Arrays;
+
+public class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+
+        return Arrays.equals(target, arr);
+    }
+}
