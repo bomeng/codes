8c49d6c9de20d8afad02b40b7ed6215635e1de24
Bo Meng <bo.meng@sysdig.com>
2020-06-01 13:53:26 -0700

    lc

---
+++ java/leetcode/src/solution_1461/Solution.java
@@ -0,0 +1,23 @@
+package solution_1461;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        Set<String> set = new HashSet<>();
+        int target = (int) Math.pow(2, k);
+        for (int i = 0, n = s.length() - k; i <= n; i++) {
+            set.add(s.substring(i, i + k);
+            if (set.size() == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.hasAllCodes("00110110", 2));
+    }
+}
