54447b49b93d37c1b18da07f662ac193b48b271a
Bo Meng <bo.meng@sysdig.com>
2020-06-01 21:32:50 -0700

    lc

---
+++ java/leetcode/src/solution_1025/Solution.java
@@ -0,0 +1,11 @@
+package solution_1025;
+
+public class Solution {
+    public boolean divisorGame(int N) {
+        if (N % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
