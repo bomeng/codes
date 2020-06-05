e1903248558fb9cf36c91352520d9b313eda9f0a
Bo Meng <bo.meng@sysdig.com>
2020-06-05 08:47:35 -0700

    lc

---
+++ java/leetcode/src/solution_459/Solution.java
@@ -0,0 +1,8 @@
+package solution_459;
+
+public class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String a = s + s;
+        return a.substring(1, a.length() - 1).contains(s);
+    }
+}
