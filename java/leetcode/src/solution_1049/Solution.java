2f3e3c8fbaed0a42f787410b3e50cdc78a245949
Bo Meng <bo.meng@sysdig.com>
2020-06-07 12:07:38 -0700

    lc

---
+++ java/leetcode/src/solution_1049/Solution.java
@@ -0,0 +1,24 @@
+package solution_1049;
+
+public class Solution {
+    public int lastStoneWeightII(int[] stones) {
+        int n = stones.length;
+        int total = 0;
+        for (int i = 0; i < stones.length; i++)
+            total += stones[i];
+        int S2 = 0;
+        boolean[] dp = new boolean[total+1];  // use only 1D array to store the status
+        dp[0] = true;
+        for (int s: stones) {
+            boolean[] cur = dp.clone();
+            for (int i = s; i <= total/2; i++) {  // wont affect the value that is smaller than current stone's weight
+                if (dp[i - s]) {
+                    cur[i] = true;
+                    S2 = Math.max(S2, i);
+                }
+            }
+            dp = cur;
+        }
+        return total - 2 * S2;
+    }
+}
