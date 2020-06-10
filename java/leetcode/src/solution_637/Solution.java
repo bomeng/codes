9029439685fb286782c1bdbd9ffef404d95814ba
Bo Meng <bo.meng@sysdig.com>
2020-06-10 11:17:15 -0700

    lc

---
+++ java/leetcode/src/solution_637/Solution.java
@@ -0,0 +1,38 @@
+package solution_637;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) {
+        this.val =val;
+    }
+}
+
+public class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> list =new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum = sum + node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            list.add(sum / size);
+        }
+        return list;
+    }
+}
