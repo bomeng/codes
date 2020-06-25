3c2af09d2a3dc71f1d1e55829c541ed0f75bcfd4
Bo Meng <bmeng13@bloomberg.net>
2020-06-25 07:52:29 -0700

    lc

---
+++ java/leetcode/src/solution_287/Solution.java
@@ -0,0 +1,23 @@
+package solution_287;
+
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        //FLOYD HARE & TORTOISE ALGO
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        // FINDING STARTING POINT OF CYCLE
+        int start = nums[0];
+        while (start != slow) {
+            start = nums[start];
+            slow = nums[slow];
+        }
+        return start;
+    }
+}
