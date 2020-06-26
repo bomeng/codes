4da16d718b67359cd11171b4cdc337bfb19447a2
Bo Meng <bmeng13@bloomberg.net>
2020-06-25 18:54:58 -0700

    lc

---
+++ java/leetcode/src/solution_1486/Solution.java
@@ -0,0 +1,11 @@
+package solution_1486;
+
+public class Solution {
+    public int xorOperation(int n, int start) {
+        int result = start;
+        for (int i = 1; i < n; i++) {
+            result = result ^ (start + 2 * i);
+        }
+        return result;
+    }
+}
