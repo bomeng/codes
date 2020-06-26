5a4a0de2f7fec488bfca6dd4f694938289ac5e20
Bo Meng <bmeng13@bloomberg.net>
2020-06-25 20:46:19 -0700

    lc

---
+++ java/leetcode/src/solution_1476/SubrectangleQueries.java
@@ -0,0 +1,20 @@
+package solution_1476;
+
+import java.util.Arrays;
+
+public class SubrectangleQueries {
+    int[][] rectangles;
+
+    public SubrectangleQueries(int[][] rectangle) {
+        rectangles = Arrays.copyOf(rectangle, rectangle.length);
+    }
+
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (int row = row1; row <= row2; row++) {
+            Arrays.fill(rectangles[row], col1, col2 + 1, newValue);
+        }
+    }
+
+    public int getValue(int row, int col) {
+        return rectangles[row][col];
+}
