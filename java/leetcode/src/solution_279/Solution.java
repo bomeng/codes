57479be031bf71684d889d285fd8921e5bc3e007
Bo Meng <bmeng13@bloomberg.net>
2020-06-27 22:51:08 -0700

    lc

--- java/leetcode/src/solution_279/Solution.java
+++ java/leetcode/src/solution_279/Solution.java
@@ -1,28 +1,25 @@
 package solution_279;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
     public int numSquares(int n) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] map = new int[n + 1];
         return numSquares(n, map);
     }
 
-    private int numSquares(int n, Map<Integer, Integer> map) {
-        if (map.containsKey(n)) {
-            return map.get(n);
+    private int numSquares(int n, int[] map) {
+        if (map[n] > 0) {
+            return map[n];
         }
         double s = Math.sqrt(n);
         if (s == (int) s) {
-            map.put(n, 1);
+            map[n] = 1;
             return 1;
         }
         int min = Integer.MAX_VALUE;
         for (int i = 1; i * i <= n; i++) {
             min = Math.min(min, numSquares(n - i * i, map) + 1);
         }
-        map.put(n, min);
+        map[n] = min;
         return min;
     }
 }
